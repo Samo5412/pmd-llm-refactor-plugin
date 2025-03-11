f163b99dfa2873ed8196c7371d45dc73a0684c3a
Sara Moussa <127226713+Samo9721@users.noreply.github.com>
2025-03-11 18:58:54 +0100

    tech: add Log4j 2 dependencies for logging

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,6 +41,8 @@ dependencies {
     implementation("org.apache.httpcomponents.client5:httpclient5:5.2.1")
     implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.20.0")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("org.apache.logging.log4j:log4j-api:2.20.0")
+    implementation("org.apache.logging.log4j:log4j-core:2.20.0")
 }
 
 // IntelliJ plugin configuration
